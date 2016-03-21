885bd2055c3cb03fadb9251c7f051d5ad048d2f4
cheng yin <wyyincheng@163.com>
2016-03-21 13:54:29 +0800

    Initial Commit

---
+++ ycOCApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ycOCApp
+//
+//  Created by cheng yin on 16/3/21.
+//  Copyright © 2016年 cheng yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ycOCApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ycOCApp
+//
+//  Created by cheng yin on 16/3/21.
+//  Copyright © 2016年 cheng yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
